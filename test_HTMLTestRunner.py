730bb81ec66115dc869b7f0b8e4306383cc5c0c6
sugera <46101151+sugera@users.noreply.github.com>
2020-05-19 17:47:25 +0800

    Add files via upload

---
+++ test_HTMLTestRunner.py
@@ -0,0 +1,53 @@
+import unittest
+from HTMLTestRunner import HTMLTestRunner
+
+
+class TestDemo(unittest.TestCase):
+    """测试用例说明"""
+
+    def test_success(self):
+        """执行成功"""
+        self.assertEqual(2 + 3, 5)
+
+    @unittest.skip("skip case")
+    def test_skip(self):
+        pass
+
+    def test_fail(self):
+        self.assertEqual(5, 6)
+
+    def test_error(self):
+        self.assertEqual(a, 6)
+
+
+class TestDemo2(unittest.TestCase):
+
+    def test_success(self):
+        self.assertEqual(2 + 2, 4)
+
+
+class TestDemo3(unittest.TestCase):
+
+    def test_fail(self):
+        self.assertEqual(3, 4)
+
+
+if __name__ == '__main__':
+    suit = unittest.TestSuite()
+    # suit.addTest(TestDemo("test_success"))
+    # suit.addTest(TestDemo("test_skip"))
+    suit.addTest(TestDemo("test_fail"))
+    suit.addTest(TestDemo("test_error"))
+    suit.addTest(TestDemo2("test_success"))
+    suit.addTest(TestDemo3("test_fail"))
+
+    # with(open('./result.html', 'wb')) as fp:
+    #     runner = HTMLTestRunner(
+    #         stream=fp,
+    #         title='Seldom自动化测试报告',
+    #         description='浏览器chrome，平台windows'
+    #     )
+    fp = open("./result.html", "wb")
+    runner = HTMLTestRunner(stream=fp, title="测试百度搜索",description = "笑笑的电脑")
+    runner.run(suit)  # 调用run()运行测试套件
+    # runner.run(suit)
